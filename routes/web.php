abf90f1b7a02b4b050ceaf11ea2ad0c4516ee15f
3A632011 <abc4173727@gmail.com>
2019-11-22 02:07:45 +0800

    設定 Route 前置

--- routes/web.php
+++ routes/web.php
@@ -24,8 +24,17 @@ Route::get('say/{name?}',['as'=>'hello.index',function($name='Everybody'){
     return view('welcome');
 }]);
 
-//Route::get('hi/{name?}',function($name = 'Everybody'){
- //   return'hi,'.$name;
-//});
+Route::get('hi/{name?}',function($name = 'Everybody'){
+    return'hi,'.$name;
+});
 
+Route::get('dashboard',function(){
+    return'dashboard';
+});
+
+Route::group(['prefix'=>'admin'],function(){
+    Route::get('dashboard',function(){
+        return'admin dashboard';
+    });
+});
 
